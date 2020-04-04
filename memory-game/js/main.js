54bee8af064d0ba2cac47b252694780d3258bf8e
Adlan <adlanyandarbiev@gmail.com>
2020-04-04 10:33:21 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,10 +1,20 @@
-console.log("Up and running!");
 
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped" + " " + cardOne);
 console.log("User flipped" + " " + cardTwo);
+
+for (let i = 0; i < cardsInPlay.length; i += 1) {
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!")
+    } else {
+        alert("Sorry, try again!");
+    }
+} 
