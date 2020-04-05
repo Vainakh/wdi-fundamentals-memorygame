c999155b43fc64ceca9f00aefaf081371816be7f
Adlan <adlanyandarbiev@gmail.com>
2020-04-05 09:09:00 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,27 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+        },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+        },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+        },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
+
 let cardsInPlay = [];
 
 
@@ -7,17 +29,20 @@ function checkForMatch(){
     
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
-            return console.log("You found a match!");
+            return alert("You found a match!");
         } else {
-            return console.log("Sorry, try again.");
+            return alert("Sorry, try again.");
         }
     }  
 }
 
 function flipCard(cardId){
     
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+
     
     checkForMatch();
 }
